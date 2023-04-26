31907fc13672bb38776e80c8c85aa80d3ffbb703
santi <santiago.torresv>
2023-04-26 14:04:06 -0500

    Diseño estético sin funcionalidades del sign up

---
+++ app/src/main/java/com/santiagotorres/clothify/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.santiagotorres.clothify.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santiagotorres.clothify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//prueba git
--- app/src/main/java/com/santiagotorres/clothify/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/santiagotorres/clothify/ui/signup/SignUpActivity.kt
@@ -1,13 +1,12 @@
-package com.santiagotorres.clothify
+package com.santiagotorres.clothify.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.santiagotorres.clothify.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
-
-//prueba git
