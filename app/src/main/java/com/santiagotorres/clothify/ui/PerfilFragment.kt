4c94a8bf062888f8860bc8bdac3b93779726fb13
santi <santiago.torresv>
2023-05-16 00:56:05 -0500

    Se deja funcionando la recuperación de contraseña con su debido flujo, con esto se termina toda la funcionalidad del sign in y el sign up, se deja parte de avance en el fragmento del perfil x2

--- app/src/main/java/com/santiagotorres/clothify/ui/PerfilFragment.kt
+++ app/src/main/java/com/santiagotorres/clothify/ui/PerfilFragment.kt
@@ -1,15 +1,19 @@
 package com.santiagotorres.clothify.ui
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.santiagotorres.clothify.Manifest
 import com.santiagotorres.clothify.databinding.FragmentPerfilBinding
 import com.santiagotorres.clothify.ui.signin.SignInActivity
 
@@ -46,7 +50,11 @@ class PerfilFragment : Fragment() {
             activity?.finish()
         }
 
+
+
         return view
     }
+
+
 
 }
