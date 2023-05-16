4ede75e988168f5ac6ea24cb7c6e09ab657f1e52
santi <santiago.torresv>
2023-05-16 01:02:52 -0500

    Se elimina el correo y contraseña en rojo de ayuda para ingresar, pues ya todo funciona con bases de datos.

--- app/src/main/java/com/santiagotorres/clothify/ui/PerfilFragment.kt
+++ app/src/main/java/com/santiagotorres/clothify/ui/PerfilFragment.kt
@@ -1,19 +1,16 @@
 package com.santiagotorres.clothify.ui
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.santiagotorres.clothify.Manifest
 import com.santiagotorres.clothify.databinding.FragmentPerfilBinding
 import com.santiagotorres.clothify.ui.signin.SignInActivity
 
